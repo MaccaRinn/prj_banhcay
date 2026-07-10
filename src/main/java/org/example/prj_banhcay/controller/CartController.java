fed40e8c20efdc80ee629832c2ef9b4f2c6456d5
MaccaRinn <nghoangvietanh382004@gmail.com>
2026-07-10 21:38:55 +0700

    update cart api

--- src/main/java/org/example/prj_banhcay/controller/CartController.java
+++ src/main/java/org/example/prj_banhcay/controller/CartController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cart")
-@CrossOrigin(origins = "*", allowCredentials = "true")
+@CrossOrigin(origins = "https://fe-nguvi.vercel.app", allowCredentials = "true")
 @RequiredArgsConstructor
 public class CartController {
     @Autowired
